4f65b152b37be170c8c5d578b3f66f4ecab26fc2
marianalcar <mariana.leite.de.carvalho@tecnico.ulisboa.pt>
2023-01-12 14:22:11 +0000

    subscriber conditions

--- subscriber/sub.c
+++ subscriber/sub.c
@@ -1,9 +1,15 @@
 #include "logging.h"
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: sub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
+    char register_pipe_name[256];
+    char pipe_name[256];
+    char box_name[256]; 
+    if (argc != 4) {
+        fprintf(stderr, "usage: sub <register_pipe_name> <box_name>\n");
+        return -1;
+    }
+    strncpy(register_pipe_name,argv[1],256);
+    strncpy(pipe_name,argv[2],256);
+    strncpy(box_name,argv[3],256);
     return -1;
 }
