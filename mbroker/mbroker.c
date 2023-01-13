deb94e1da61f95658cfcf2078bb4394631151c06
Guilherme da Silva Sande <guilhermesande@MacBook-Pro-de-Guilherme.local>
2023-01-13 01:12:46 +0000

    mbroker in progress

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,9 +1,57 @@
 #include "logging.h"
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    char register_pipe_name[256];
+    int max_sessions;
+    char char_code;
+    int code;
+
+    if (unlink(pipe_name) != 0 && errno != ENOENT) {
+        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", pipe_name,
+                strerror(errno));
+        return -1;
+    }
+
+    // create pipe
+    if (mkfifo(pipe_name, 0640) != 0) {
+        fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // open pipe for writing
+    int rx = open(pipe_name, O_RDONLY);
+    if (rx == -1) {
+        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+        return -1;
+    }
+    while (1) {
+        read(rx,char_code,1);
+        code = atoi(char_code);
+        switch(code) {
+            case 1:
+                
+            case 2:
+
+            case 3:
+
+            case 4:
+
+            case 5:
+
+            case 6:
+
+            case 7:
+
+            case 8:
+                
+        }
+    }
+
 }
